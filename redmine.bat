5d5f9bec7b22d13454cc070d1b48d496a2acddc6
n-kats <n-kats19890214@hotmail.co.jp>
2014-07-28 18:37:49 +0900

    fix variable name

--- redmine.bat
+++ redmine.bat
@@ -1,6 +1,6 @@
 @echo off
-set READMINEPATH=C:\usr\local\lib\redmine-2.5.2
-pushd %READMINEPATH%
+set READMINE_PATH=C:\usr\local\lib\redmine-2.5.2
+pushd %READMINE_PATH%
 bundle exec ruby script/rails server webrick -e production --port=3010
 
 pause
